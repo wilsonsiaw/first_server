5bb6295a8ec8623fd7329683b7a16f963bd307d6
Wilson Siaw <wilsonsiawse@gmail.com>
2023-12-29 17:36:28 +0200

    ✨ feat(Server): Set Content Type
    Set the initial content type of files to be served up by the server

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const server = http.createServer((req, res) => {
     let filePath = path.join(__dirname, "public", req.url === "/" ? "index.html" : req.url);
 });
 
+// set the initial content type of files
+let contentType = "text/html";
+
+
+
 // set the port based on available ports on user machine
 const PORT = process.env.PORT || 8080;
 
