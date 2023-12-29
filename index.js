147fdb3f550a0050b8869e4958a541a4e960a429
Wilson Siaw <wilsonsiawse@gmail.com>
2023-12-29 17:48:07 +0200

    ✨ feat(Server): Set Content Type
    Set the content type of each file extension that the server serves up

--- index.js
+++ index.js
@@ -14,7 +14,24 @@ const server = http.createServer((req, res) => {
     // get the extension of each file in the file path
     let extname = path.extname(filePath);
 
-
+    // check the extension and set the corresponding content type
+    switch (extname) {
+        case ".js":
+            contentType = "text/javascript";
+            break;
+        case ".css":
+            contentType = "text/css";
+            break;
+        case ".json":
+            contentType = "application/json";
+            break;
+        case ".png":
+            contentType = "image/png";
+            break;
+        case ".jpg":
+            contentType = "image/png";
+            break;
+    }
 });
 
 // set the port based on available ports on user machine
