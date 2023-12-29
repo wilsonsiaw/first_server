cb2912bd2003e6c9964ce154b09abb804ed5671d
Wilson Siaw <wilsonsiawse@gmail.com>
2023-12-29 17:43:04 +0200

    🦄 refactor(Server): Adjust Code
    Put the code inside the server

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ const path = require("path");
 const server = http.createServer((req, res) => {
     // build file paths for each file to be served
     let filePath = path.join(__dirname, "public", req.url === "/" ? "index.html" : req.url);
+
+    // set the initial content type of files
+    let contentType = "text/html";
+
+    // get the extension of each file in the file path
+    let extname = path.extname(filePath);
+
+
 });
-
-// set the initial content type of files
-let contentType = "text/html";
-
-// get the extension of each file in the file path
-let extname = path.extname(filePath);
 
 // set the port based on available ports on user machine
 const PORT = process.env.PORT || 8080;
