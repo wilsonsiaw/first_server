b7673726d147253d7eba436bec8733b75585ab01
Wilson Siaw <wilsonsiawse@gmail.com>
2023-12-29 17:31:07 +0200

    ✨ feat(Server): Initialize server
    Create a server and listen for it on the requested port

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
+// create a server
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+})
+
+// set the port based on available ports on user machine
+const PORT = process.env.PORT || 8080;
+
+// listen on the server
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
